85e01b2146d159c8a9d8ee37d636cf64369e2c66
Chaofanman <gchase.patron@gmail.com>
2016-05-29 15:49:09 +0800

    all functions finished and working

--- blackjack.go
+++ blackjack.go
@@ -16,13 +16,12 @@ type Player struct {
 }
 
 type Deck []Card
-type House []Card
-type User []Card
 
 func newDeck() (deck Deck) {
 	Value := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13}
 	Suit := []string{"Spades", "Clubs", "Diamonds", "Hearts"}
 
+	//Initialize every card in the deck
 	for i := 0; i < len(Value); i++ {
 		for j := 0; j < len(Suit); j++ {
 			card := Card{Value[i], Suit[j]}
@@ -41,21 +40,47 @@ func Shuffle(deck Deck) {
 	}
 }
 
-func Deal(deck Deck, player Player, count *int) (card, Card) {
+//Pass pointer to be able to dereference
+func Deal(deck Deck, count *int) (card Card) {
 	currentCard := deck[*count]
-	player = append(player, currentCard)
 	*count++
 	return currentCard
 }
 
+//Allows to dynamically add to the Player Hand
+//which is an array of cards
+func (player *Player) addCard(card Card) []Card {
+	player.Hand = append(player.Hand, card)
+	return player.Hand
+}
+
+func (player *Player) countScore() int {
+	for i := 0; i < len(player.Hand); i++ {
+		player.Score += player.Hand[i].Value
+	}
+	return player.Score
+}
+
 func main() {
+	//Initialize new deck using a function
 	deck := newDeck()
 	Shuffle(deck)
 	count := 0
 
-	user := User
+	newCard := Deal(deck, &count)
+	newCard2 := Deal(deck, &count)
 
-	Deal(deck, User, &count)
-	fmt.Printf("%+v | count: %d\n", topCard, count)
+	fmt.Println(newCard)
+
+	//Initialize a struct Player
+	user := Player{}
+
+	user.addCard(newCard)
+	user.addCard(newCard2)
+
+	user.countScore()
+
+	fmt.Println(user.Hand)
+	fmt.Println(user.Score)
 
 }
